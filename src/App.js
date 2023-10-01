b3b5953e904adb17de76b02725e45f06b984926c
vishnuu96 <riyachowdary37@gmail.com>
2023-10-01 22:01:52 +0530

    added cart reducer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Layout/Header";
 
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
 
@@ -15,13 +16,13 @@ function App() {
  }
 
   return (
-    <Fragment>
+    <CartProvider>
     {cartIsShown && <Cart onClose={hideCartHandler}/>}
     <Header onShowCart={cartShowhandler} />
     <main>
       <Meals />
     </main>
-  </Fragment>
+  </CartProvider>
   );
 }
 
